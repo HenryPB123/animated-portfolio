94a95cdd3d87604b7bb6be5e022de250fb753751
Henry P B <mahe2di@gmail.com>
2023-10-20 22:00:40 -0500

    Change 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import "./app.scss";
+
 function App() {
-  return <div>Hola desde el nuevo portfolio</div>;
+  return (
+    <div>
+      <section>
+        <a href="">Hero</a>
+      </section>
+      <section>Paralax</section>
+      <section>Services</section>
+      <section>Paralax</section>
+      <section>Porfolio1</section>
+      <section>Porfolio2</section>
+      <section>Porfolio3</section>
+      <section>Contact</section>
+    </div>
+  );
 }
 
 export default App;
